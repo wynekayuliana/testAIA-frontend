d1983b17bbbb7387f1e2733dae2520795bb9153f
eka <app6@tjendana-corporate.com>
2020-12-05 21:00:53 +0800

    setup display using material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { TextField, Divider, InputAdornment, IconButton } from '@material-ui/core';
+import ImageSearchIcon from '@material-ui/icons/ImageSearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getFormData(e) {
+  const data = {};
+  const formData = new FormData(e.target);
+  for (let entry of formData.entries()) {
+    data[entry[0]] = entry[1]
+  }
+  return data;
 }
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  onSubmitSearch(e) {
+    e.preventDefault();
+    var data = getFormData(e.target);
+
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <h2 style={{ margin: "1rem 0" }}>Test AIA Project</h2>
+          <form onSubmit={this.onSubmitSearch}>
+            <div className="h-flex jc ac search-bar">
+              <TextField id="search_value" name="search_value"
+                label="Search Something . . ."
+                variant="outlined" className="search-input"
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">
+                    <IconButton aria-label="ImageSearch" title="click to search" type="submit">
+                      <ImageSearchIcon />
+                    </IconButton>
+                  </InputAdornment>,
+                }}
+              />
+            </div>
+          </form>
+        </div>
+        <Divider />
+        <div className="app-content">
+          tes
+        </div>
+      </div>
+    )
+  }
+}
